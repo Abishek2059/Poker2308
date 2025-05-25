a6986cdbfbc99e4737e53d2643a0ea5800a85b00
vsa31 <vsa31@txstate.edu>
2025-05-25 19:44:32 -0400

    Fix deck creation and card push issue

---
+++ build/CMakeFiles/Poker.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poker.dir/main.cpp.obj"
+  "CMakeFiles/Poker.dir/main.cpp.obj.d"
+  "CMakeFiles/Poker.dir/src/cards.cpp.obj"
+  "CMakeFiles/Poker.dir/src/cards.cpp.obj.d"
+  "Poker.exe"
+  "Poker.exe.manifest"
+  "Poker.pdb"
+  "libPoker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
